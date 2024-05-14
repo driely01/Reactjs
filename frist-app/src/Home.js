797677cdd0cd5595f59e5f9356b7f751fe91daf7
driely01 <elyaagoubi.works@gmail.com>
2024-05-14 21:07:09 +0100

    add conditional loading with animation

--- frist-app/src/Home.js
+++ frist-app/src/Home.js
@@ -1,19 +1,27 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import Loading from './Loading';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
+	const [isPending, setIsPending] = useState(true);
 	const handleDelete = (id) => {
 		const newBlogs = blogs.filter(blog => blog.id !== id)
 		setBlogs(newBlogs);
 	};
 	useEffect(() => {
-		fetch('http://localhost:8000/blogs')
+		setTimeout(() => { // just simulating the fetch large data.
+			fetch('http://localhost:8000/blogs')
 			.then(res => res.json())
-			.then(data => setBlogs(data));
+			.then(data => {
+				setBlogs(data);
+				setIsPending(false);
+			});
+		}, 1000)
 	}, []);
 	return (
 		<div className="home">
+			{isPending && <Loading /> }
 			{blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>}
 		</div>
 	);
---
+++ frist-app/src/Loading.js
@@ -0,0 +1,9 @@
+import './loading.css'
+const Loading = () => {
+	return (
+		<div className="ring">Loading
+			<span className="point"></span>
+		</div>
+	);
+}
+export default Loading;
