de22395b692661551c6e7a9b817f7fda24578bf8
driely01 <elyaagoubi.works@gmail.com>
2024-05-14 16:56:15 +0100

    learn function as props

--- frist-app/src/BlogList.js
+++ frist-app/src/BlogList.js
@@ -1,11 +1,13 @@
-const BlogList = ({blogs}) => {
+const BlogList = ({blogs, title, handleDelete}) => {
 	return (
 		<div className="blog-list">
+			<h2 className="blog-title"><span>{title}</span></h2>
 			{blogs.map((blog) => (
 				<div className="blog-preview" key={blog.id}>
 					<h2>{blog.title}</h2>
-					<p className="blog-author">Written by {blog.author}</p>
-					<div className="blog-body">{blog.body}</div>
+					<p className="blog-author">Written by <span>{blog.author}</span></p>
+					<p className="blog-body">{blog.body}</p>
+					<button onClick={() => handleDelete(blog.id)}>delete blog</button>
 				</div>
 			))}
 		</div>
--- frist-app/src/Home.js
+++ frist-app/src/Home.js
@@ -2,14 +2,18 @@ import { useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-	const [blogs] = useState([
+	const [blogs, setBlogs] = useState([
 		{title: 'My new website', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, elit vitae pellentesque sagittis, erat est blandit libero, sit amet tristique sapien erat et justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam accumsan, dui ac fermentum rutrum, purus urna malesuada turpis, eu accumsan metus odio eu lectus. Quisque gravida blandit dolor sit amet malesuada. Curabitur ornare ipsum quis justo vulputate efficitur. Pellentesque blandit sodales tellus porttitor scelerisque. Maecenas vitae euismod lorem. Quisque consectetur nulla nunc, ac interdum justo tincidunt nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla dolor arcu, varius eu diam et, vestibulum consectetur diam.', author: 'mario', id: 1},
 		{title: 'Welcome party!', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, elit vitae pellentesque sagittis, erat est blandit libero, sit amet tristique sapien erat et justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam accumsan, dui ac fermentum rutrum, purus urna malesuada turpis, eu accumsan metus odio eu lectus. Quisque gravida blandit dolor sit amet malesuada. Curabitur ornare ipsum quis justo vulputate efficitur. Pellentesque blandit sodales tellus porttitor scelerisque. Maecenas vitae euismod lorem. Quisque consectetur nulla nunc, ac interdum justo tincidunt nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla dolor arcu, varius eu diam et, vestibulum consectetur diam.', author: 'driss', id: 2},
-		{title: 'Web dev top tips', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, elit vitae pellentesque sagittis, erat est blandit libero, sit amet tristique sapien erat et justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam accumsan, dui ac fermentum rutrum, purus urna malesuada turpis, eu accumsan metus odio eu lectus. Quisque gravida blandit dolor sit amet malesuada. Curabitur ornare ipsum quis justo vulputate efficitur. Pellentesque blandit sodales tellus porttitor scelerisque. Maecenas vitae euismod lorem. Quisque consectetur nulla nunc, ac interdum justo tincidunt nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla dolor arcu, varius eu diam et, vestibulum consectetur diam.', author: 'john', id: 3},
+		{title: 'Web dev top tips', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus, elit vitae pellentesque sagittis, erat est blandit libero, sit amet tristique sapien erat et justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam accumsan, dui ac fermentum rutrum, purus urna malesuada turpis, eu accumsan metus odio eu lectus. Quisque gravida blandit dolor sit amet malesuada. Curabitur ornare ipsum quis justo vulputate efficitur. Pellentesque blandit sodales tellus porttitor scelerisque. Maecenas vitae euismod lorem. Quisque consectetur nulla nunc, ac interdum justo tincidunt nec. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla dolor arcu, varius eu diam et, vestibulum consectetur diam.', author: 'mario', id: 3},
 	]);
+	const handleDelete = (id) => {
+		const newBlogs = blogs.filter(blog => blog.id !== id)
+		setBlogs(newBlogs);
+	};
 	return (
 		<div className="home">
-			<BlogList blogs={blogs} title='All Blogs'/>
+			<BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
 		</div>
 	);
 }
