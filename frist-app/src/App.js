054f77b55533b4a21e2092cc286ddd0bcab63805
driely01 <elyaagoubi.works@gmail.com>
2024-05-14 12:38:50 +0100

    style things

--- frist-app/src/App.js
+++ frist-app/src/App.js
@@ -1,19 +1,12 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-	const title = 'Welcome to the new blog';
-	const likes = 50;
-	const link = 'http://www.google.com';
 	return (
 		<div className="App">
+			<Navbar />
 			<div className="content">
-				<h1>{title}</h1>
-				<p>Liked {likes} times</p>
-				<p>{20 + 10}</p>
-				<p>{'hello world'}</p>
-				<p>{[1, 2, 3, 4, 5]}</p>
-				<p>{Math.random() * 10}</p>
-				<a href={link}>google link</a>
+				<Home />
 			</div>
 		</div>
 	);
---
+++ frist-app/src/Home.js
@@ -0,0 +1,8 @@
+const Home = () => {
+	return (
+		<div className="home">
+			<h2>home page</h2>
+		</div>
+	);
+}
+export default Home;
