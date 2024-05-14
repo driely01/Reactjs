ffa35e7dc8a05506d7512b25b60a8f50a8e2f2ad
driely01 <elyaagoubi.works@gmail.com>
2024-05-14 14:42:08 +0100

    learn useState hook

--- frist-app/src/Home.js
+++ frist-app/src/Home.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 const Home = () => {
+	const [name, setName] = useState('mario');
+	const [age, setAge] = useState(25);
+	const handleClick = () => {
+		setName('driss');
+		setAge(22);
+	};
 	return (
 		<div className="home">
 			<h2>home page</h2>
+			<p>{name} is {age} years old.</p>
+			<button className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded" onClick={handleClick}>Click me</button>
 		</div>
 	);
 }
