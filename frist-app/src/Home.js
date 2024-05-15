b9270a892dba51b4f065f1d6316a3608bf13f59c
driely01 <elyaagoubi.works@gmail.com>
2024-05-15 11:26:57 +0100

    making a custom hook

--- frist-app/src/Home.js
+++ frist-app/src/Home.js
@@ -1,34 +1,13 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import Loading from './Loading';
+import useFetch from './useFetch';
 
 const Home = () => {
-	const [blogs, setBlogs] = useState(null);
-	const [isPending, setIsPending] = useState(true);
-	const [error, setError] = useState(null);
+	const {data: blogs, setData:setBlogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 	const handleDelete = (id) => {
-		const newBlogs = blogs.filter(blog => blog.id !== id)
+		const newBlogs = blogs.filter(data => data.id !== id)
 		setBlogs(newBlogs);
 	};
-	useEffect(() => {
-		setTimeout(() => { // just simulating the fetch large data.
-			fetch('http://localhost:8000/blogs')
-			.then((res) => {
-				if (!res.ok)
-					throw Error('could not fetch the data for that resource');
-				return res.json();
-			})
-			.then(data => {
-				setBlogs(data);
-				setIsPending(false);
-				setError(null);
-			})
-			.catch(err => {
-				setError(err.message);
-				setIsPending(false);
-			});
-		}, 1000)
-	}, []);
 	return (
 		<div className="home">
 			{error && <div>{error}</div> }
---
+++ frist-app/src/useFetch.js
@@ -0,0 +1,29 @@
+import {useState, useEffect} from 'react';
+
+const useFetch = url => {
+	const [data, setData] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
+	useEffect(() => {
+		setTimeout(() => { // just simulating the fetch large data.
+			fetch(url)
+				.then((res) => {
+						if (!res.ok)
+								throw Error('could not fetch the data for that resource');
+					return res.json();
+				})
+				.then(data => {
+					setData(data);
+					setIsPending(false);
+					setError(null);
+				})
+				.catch(err => {
+					setError(err.message);
+					setIsPending(false);
+				});
+		}, 1000)
+	}, [url]);
+	return {data, setData, isPending, error};
+}
+
+export default useFetch;
